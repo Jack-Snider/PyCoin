2ce96020549be5f3d9bd3ae505f18d52f828923f
= <choiman2021@gmail.com>
2024-04-03 07:31:07 +0900

    login form

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('login/', LoginView.as_view(template_name = "/common/login.html"), name = "login"),
+    path('logout/', LogoutView.as_view(), name = "logout"),
 ]
